12760a446312905952af46c8f3f2318b128e658c
Finn Döbele <finn.doebele@bluewin.ch>
2024-05-01 20:39:15 +0200

    added button feature, that it wont draw points anymore

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 const canvas = document.getElementById('drawingArea');
 const ctx = canvas.getContext('2d');
 const clearButton = document.getElementById('clearButton');
-//const visualizeON3Button = document.getElementById('visualizeON3');
-//const visualizeONhButton = document.getElementById('visualizeONh');
+const visualizeON3Button = document.getElementById('visualizeON3');
+const visualizeONhButton = document.getElementById('visualizeONh');
 
 let points = []; //to store the coords of the points
+let visualizationInProgress = false; //prevent drawing points during visualization
 
 canvas.addEventListener('click', function(event) {
-    const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
-    drawPoint(x, y);
-    points.push({x, y});
-    localStorage.setItem('points', JSON.stringify(points)); //save the points in the local storage (as strings)
+    if (!visualizationInProgress) {
+        const rect = canvas.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        drawPoint(x, y);
+        points.push({x, y});
+        localStorage.setItem('points', JSON.stringify(points)); //save the points in the local storage (as strings)
+    }
 });
 
 clearButton.addEventListener('click', function() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     points = [];
     localStorage.removeItem('points'); //remove the points from the local storage
+    visualizationInProgress = false; //stop visualization
+    visualizeON3Button.disabled = false; //enable the buttons
+    visualizeONhButton.disabled = false;
 });
 
 function drawPoint(x, y) {
@@ -27,12 +33,14 @@ function drawPoint(x, y) {
     ctx.fill();
 }
 
-/*
+// Code to visualize convex hull in O(n^3)
 visualizeON3Button.addEventListener('click', function() {
-    // Code to visualize convex hull in O(n^3)
+    visualizationInProgress = true;
+    visualizeONhButton.disabled = true; //disable the other button
 });
 
+// Code to visualize convex hull in O(nh)
 visualizeONhButton.addEventListener('click', function() {
-    // Code to visualize convex hull in O(nh)
+    visualizationInProgress = true;
+    visualizeON3Button.disabled = true; //disable the other button
 });
-*/
